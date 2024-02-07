441e8438226a910e107ed07716b9f944178de9a6
obranen <krakhnytskyi@gmail.com>
2024-02-07 17:25:55 +0200

    update

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,16 @@
+import Image from 'next/image'
+
 export default function Home() {
   return (
     <>
-      Home
+      <Image
+        src="/image/main/spikelet.jpg"
+        width={608}
+        height={327}
+        alt="spikelet"
+        priority
+        className={'absolute top-0 left-0 w-[608px] h-[327px]'}
+      />
     </>
   )
 }
--- frontend/components/Header/Header.tsx
+++ frontend/components/Header/Header.tsx
@@ -1,17 +1,8 @@
-import Image from 'next/image'
 import Navigation from '@/components/Header/Navigation/Navigation'
 
 const Header = () => {
   return (
     <div className={'bg-myPrimaryLight dark:bg-myPrimaryDark'}>
-      <Image
-        src="/image/main/spikelet.jpg"
-        width={608}
-        height={327}
-        alt="spikelet"
-        priority
-        className={'absolute w-[608px] h-[327px]'}
-      />
       <div className={'container'}>
         <Navigation/>
       </div>
